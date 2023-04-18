9e0bd4f8c2ade62bb9a68c9f2a8b83c7426c693d
kishk <mrsaytama000@gmail.com>
2023-04-18 02:25:31 +0200

    backEndWityhUserRegisterAndLogin

---
+++ server/models/customer.model.js
@@ -0,0 +1,139 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const joi = require('joi');//to validation schema
+
+const schemaValidationRegister = joi.object({
+    userName: joi.string().alphanum().min(2).max(20).required(),
+    email: joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password: joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    age:joi.number().required(),
+    gender: joi.string().valid('male', 'female').required(),
+    birthday: joi.string().required(),
+})
+
+const schemaValidationLogin = joi.object({
+   
+    em: joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    pass: joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    
+})
+
+let customerSchema = mongoose.Schema({
+    userName: String,
+    email: String,
+    password: String,
+    gender: String,
+    birthday: String,
+})
+
+let url = 'mongodb://localhost:27017/artecaDB'
+
+var customer = mongoose.model('customers', customerSchema);
+
+// customer register 
+exports.register = (userName,email,password,gender,birthday) => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
+           
+             //joi validation
+             let validation = await schemaValidationRegister.validateAsync({userName:userName,email:email,password:password,gender:gender,birthday:birthday})
+
+             if (validation.error) {
+
+                 mongoose.disconnect()
+                 reject(validation.error.details[0].message)
+
+         }
+         //end of validation
+            
+            return customer.findOne({email: email})
+            
+
+        }).then((doc) => {
+           
+            if (doc) {
+                mongoose.disconnect();
+                reject('This Email is exist')
+            }
+            else {
+               
+                bcrypt.hash(password, 10).then((hashPassword) => {
+                    let custom = new customer({
+                        userName:userName,
+                        email: email,
+                        password: hashPassword,
+                        gender: gender,
+                        birthday:birthday
+                        
+                    })
+                    custom.save().then((doc) => {
+                        mongoose.disconnect();
+                        resolve(custom)
+                    }).catch((error) => {
+                        mongoose.disconnect();
+                        reject(error)
+                    })
+
+                }).catch((error) => {
+                    mongoose.disconnect();
+                    reject(error)
+                })
+            }
+        })
+    })
+}
+
+var privateKey = "this is my secret key fdgioruterjekrtf,mdfgkjlsdf"
+// customer login
+exports.login =async (email,password) => {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
+           //joi validation
+    //        let validation = await schemaValidationLogin.validateAsync({ em: email, pass: password })
+
+    //        if (validation.error) {
+               
+    //            mongoose.disconnect()
+    //            reject(validation.error.details[0].message+" , and you have entered "+validation.error.details[0].context.value)
+
+    //    }
+       //end of validation
+            return customer.findOne({email: email})
+   
+        }).then((customer) => {
+           
+            if (!customer) {
+                mongoose.disconnect();
+                reject('Invalid Email or Password...')
+            }
+            else {
+                
+
+                bcrypt.compare(password, customer.password).then((same) => //to compare entered password and hashed password ecist in DB
+                {
+                    if (same) { 
+                        // send token
+                        let token = jwt.sign({ id: customer._id, userName: customer.userName }, privateKey, {
+                            expiresIn: '1h',
+
+                        })
+                        mongoose.disconnect();
+                        resolve(token)
+                    }
+                    else {
+                        mongoose.disconnect();
+                        reject('Invalid Email or Password...')
+                    }
+                }).catch((err) => {
+                    mongoose.disconnect();
+                    reject(err)
+                }) 
+            }
+        })
+    })
+}
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const customerRoute = require('./route/customer.route')
+// Using Node.js `require()`
+
+const mongoose = require('mongoose');
+
+// Allow requests from any origin
+app.use(cors());
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/', customerRoute);
+
+
+app.listen(3005, function check(error) {
+    if (error) {
+        console.log("Error...");
+    }
+    else {
+        console.log("started...");
+    }
+})
+
+// app.listen(3005);
+
+// mongoose.connect("mongodb://localhost:27017/artecaDB", { useNewUrlParser: true, useUnifiedTopology: true },
+//     function checkBD(error) {
+//         if (error) {
+//             console.log("Error connecting to DB...");
+//         }
+//         else {
+//             console.log("Successfully connecting to DB...");
+//         }
+//     }
+// )
+// mongoose.connect('mongodb://localhost/artecaDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log('Connected to MongoDB');
+// })
+// .catch((error) => {
+//   console.error('Error connecting to MongoDB:', error);
+// });
+
