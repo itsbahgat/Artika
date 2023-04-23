eea8e9fc557134d077ab14b65f6c0662f77f9ddc
kishk <mrsaytama000@gmail.com>
2023-04-23 17:28:33 +0200

    register

--- client/src/app/views/front/register/register/register.component.ts
+++ client/src/app/views/front/register/register/register.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { AuthuserService } from 'src/app/views/services/authuser.service';
+import {Router} from '@angular/router'
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  messageError: any;
+
+  constructor(private userService: AuthuserService,private rout:Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  register(f:any) {
+    let body = f.value;
+
+    this.userService.register(body).subscribe(data => {
+      console.log(data);
+
+      this.rout.navigate(['/userlogin'])
+    },(err:HttpErrorResponse)=>{this.messageError=err.error.error});
+
+
+  }
 
 }
