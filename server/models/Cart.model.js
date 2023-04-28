ca50dc47768c8ecfeb4f43d3615391add67dc8f9
Youssef king <yussefbahgat@gmail.com>
2023-04-28 12:14:53 +0300

    Remove unused code

--- server/models/Cart.model.js
+++ server/models/Cart.model.js
@@ -1,12 +1,4 @@
-// const mongoose = require("./DBconnection");
-
-// console.log(mongoose);
-
 const mongoose = require("mongoose");
-module.exports = mongoose.connect("mongodb://127.0.0.1:27017/artecaDB");
-
-
-// create schema object 
 
 const cartSchema = new mongoose.Schema({
     customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'customers', unique: true},
--- server/models/Order.model.js
+++ server/models/Order.model.js
@@ -1,13 +1,7 @@
-// const mongoose = require("./DBconnection");
-
-// console.log(mongoose);
 const mongoose = require("mongoose");
-module.exports = mongoose.connect("mongodb://127.0.0.1:27017/artecaDB");
-
-
-// create schema object 
 
 //~ The status could be "pending," "shipped," "delivered," or "cancelled."
+//    enum: ['customer', 'seller', 'admin'],
 
 const orderSchema = new mongoose.Schema({
     customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'customers' },
