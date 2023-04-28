b5a6bed08ab76fdf7316789e754645089a09ea4b
Youssef king <yussefbahgat@gmail.com>
2023-04-28 07:54:30 +0300

    Refactor product routes

--- server/route/product.route.js
+++ server/route/product.route.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const router = new express.Router();
-const ProductController = require("../Controllers/product.controller");
+const controller = require("../Controllers/product.controller");
 
-router.get("/", ProductController.getAllProducts);
+router.route("/")
+      .get(controller.getAllProducts)
+      .post(controller.addNewProduct);
 
-router.get("/:id", ProductController.getProductById);
+router.route("/:id")
+      .get(controller.getProductById)
+      .put(controller.updateProductById)
+      .delete(controller.deleteProductById);
 
-router.get("/category/:categories", ProductController.getProductsByCategory);
-
-router.get("/title/:title", ProductController.getProductsByTitle);
-
-router.post("/", ProductController.addNewProduct);
-
-router.put("/:id", ProductController.updateProductById);
-
-router.put("/review/:id", ProductController.addReviewProductById);
-
-router.delete("/:id", ProductController.deleteProductById);
-
-
+router.get("/category/:categories", controller.getProductsByCategory);
+router.get("/title/:title", controller.getProductsByTitle);
+router.put("/review/:id", controller.addReviewProductById);
 
 module.exports = router;
