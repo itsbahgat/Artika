194a7497bdc416b48a355842605494b9610fabb6
Youssef king <yussefbahgat@gmail.com>
2023-04-28 07:44:12 +0300

    Delete unused code in customer model

--- server/models/customer.model.js
+++ server/models/customer.model.js
@@ -1,27 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const joi = require('joi');//to validation schema
 
-const schemaValidationRegister = joi.object({
-    userName: joi.string().alphanum().min(2).max(20).required(),
-    email: joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    password: joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    age:joi.number().required(),
-    gender: joi.string().valid('male', 'female').required(),
-    birthday: joi.string().required(),
-})
-
-const schemaValidationLogin = joi.object({
-   
-    em: joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
-    pass: joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    
-})
 
 let customerSchema = mongoose.Schema({
     userName: String,
@@ -40,17 +20,6 @@ exports.register = (userName,email,password,gender,birthday) => {
     return new Promise((resolve, reject) => {
         mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
            
-             //joi validation
-        //      let validation = await schemaValidationRegister.validateAsync({userName:userName,email:email,password:password,gender:gender,birthday:birthday})
-
-        //      if (validation.error) {
-
-        //          mongoose.disconnect()
-        //          reject(validation.error.details[0].message)
-
-        //  }
-         //end of validation
-            
             return customer.findOne({email: email})
             
 
@@ -93,16 +62,6 @@ var privateKey = "this is my secret key fdgioruterjekrtf,mdfgkjlsdf"
 exports.login =async (email,password) => {
     return new Promise((resolve, reject) => {
         mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(async () => {
-           //joi validation
-    //        let validation = await schemaValidationLogin.validateAsync({ em: email, pass: password })
-
-    //        if (validation.error) {
-               
-    //            mongoose.disconnect()
-    //            reject(validation.error.details[0].message+" , and you have entered "+validation.error.details[0].context.value)
-
-    //    }
-       //end of validation
             return customer.findOne({email: email})
    
         }).then((customer) => {
@@ -117,7 +76,6 @@ exports.login =async (email,password) => {
                 bcrypt.compare(password, customer.password).then((same) => //to compare entered password and hashed password ecist in DB
                 {
                     if (same) { 
-                        // send token
                         let token = jwt.sign({ id: customer._id, userName: customer.userName }, privateKey, {
                             expiresIn: '1h',
 
