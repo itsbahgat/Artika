29ef893b84918947324229418de514ea7ca5d494
Youssef king <yussefbahgat@gmail.com>
2023-04-28 08:09:22 +0300

    Refactor updateProductById function

--- server/Controllers/product.controller.js
+++ server/Controllers/product.controller.js
@@ -1,24 +1,24 @@
 const productModel = require('../models/product.model');
 
-    //for all
-    const getAllProducts = async (req, res, next) => {
-        const allProducts = await productModel.find().catch((error) => {
-            next(error);
-        });
-        res.status(200).json(allProducts);
-        };
-          
-    const getProductById = async (req, res) => {
-        try {
-            const product = await productModel.findById(req.params.id);
-            if (!product) {
-            return res.status(404).json({ message: "Product not found" });
-            }
-            res.status(200).json(product);
-        } catch (error) {
-            res.status(500).json({ message: "Internal Server Error" });
+const getAllProducts = async (req, res, next) => {
+    const allProducts = await productModel.find().catch((error) => {
+        next(error);
+    });
+    res.status(200).json(allProducts);
+    };
+      
+const getProductById = async (req, res) => {
+    try {
+        const product = await productModel.findById(req.params.id);
+        if (!product) {
+        return res.status(404).json({ message: "Product not found" });
         }
-        };
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+    };
+      
     //for all
     let getProductsByCategory = async (req, res) => {
         try {
@@ -41,7 +41,6 @@ const productModel = require('../models/product.model');
     //for seller 
     let addNewProduct = async (req, res) => {
         try {
-        if (true){ //add validation function here
             const product = new productModel({
                 title : req.body.title,
                 description : req.body.description,
@@ -53,42 +52,28 @@ const productModel = require('../models/product.model');
                 });
             const newProduct = await product.save();
             res.status(201).json({ message: "Created Successfully", data: newProduct });
-        }
-        else{
-            res.status(400).json({ message: "Invalid Data" });
-        }
         } catch (error) {
         res.status(400).json({ message: error.message });
         }
     };
     //for seller
-    let updateProductById = async (req, res) => {
+    const updateProductById = async (req, res) => {
         try {
-        if (true){ //add validation function here
-            const product = await productModel.findById(req.params.id);
-            if (product) {
+            const productId = req.params.id;
+            const updatedData = req.body;
+            const updatedProduct = await productModel.findByIdAndUpdate(productId,{ $set: updatedData }, { new: true } );
 
-                product.title = req.body.title || product.title;
-                product.description = req.body.description || product.description;
-                product.price = req.body.price || product.price;
-                product.categories = req.body.categories || product.categories;
-                product.images = req.body.images || product.images;
-                product.seller = req.body.seller || product.seller;
-                product.reviews = req.body.reviews || product.reviews;
-    
-            const updatedProduct = await product.save();
-            res.status(200).json({ message: "Updated Successfully", data: updatedProduct });
-            } else {
-            res.status(404).json({ message: "invalid data" });
-            }
-        }else{
-            res.status(400).json({ message: "Invalid Data" });
-        }
-    
+            res.status(200).json({ message: "Updated Successfully"});
+          
         } catch (error) {
-        res.status(400).json({ message: error.message });
+          let isNotFound = error.name === "CastError";
+          if (isNotFound) 
+             res.status(404).json({ message: "Product is not found" });
+          else
+             res.status(400).json({ message: error.message });
         }
-    };
+      };
+      
     //for seller
     let deleteProductById = async (req, res) => {
             const id = req.params.id;
