de3467012a960fb9809b2b60a5b7b759f011aebf
Youssef king <yussefbahgat@gmail.com>
2023-04-28 07:48:46 +0300

    Update base user schema

--- server/models/customer.model.js
+++ server/models/customer.model.js
@@ -1,12 +1,51 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-let customerSchema = mongoose.Schema({
-    userName: String,
-    email: String,
-    password: String,
-    gender: String,
-    birthday: String,
-})
+const baseUserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase:true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['customer', 'seller', 'admin'],
+    default:'customer'
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps:true
+});
 
 
-module.exports = mongoose.model('customers', customerSchema);
+
+module.exports = mongoose.model('Customer', baseUserSchema);
