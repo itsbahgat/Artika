d183ffa39b0a6634968a1c74361919ade2ca831b
Youssef king <yussefbahgat@gmail.com>
2023-04-28 08:04:47 +0300

    Refactor customer routes

--- server/route/customer.route.js
+++ server/route/customer.route.js
@@ -1,23 +1,11 @@
-const route = require('express').Router();
+const express = require("express");
+const route= new express.Router();
+const controller = require("../controllers/customer.controller");
 
+route.get("/all", controller.getAllCustomers);
 
-const routeModel = require('../models/customer.model')
-
-route.post('/register', (req, res, next) => { //next : to pass the next middleware   
-    console.log(req.body)
-    routeModel.register(req.body.userName, req.body.email, req.body.password,req.body.gender,req.body.birthday)
-        .then((custom) => res.status(200).json({ custom: custom, msg: "registered" }))
-        .catch((err) => res.status(400).json({ error: err }))
-})
-
-route.post('/login', (req, res, next) => { //next : to pass the next middleware   
-    console.log(req.body)
-    routeModel.login(req.body.email, req.body.password)
-        .then((token) => res.status(200).json({ token:token }))
-        .catch((err) => res.status(400).json({ error: err }))
-})
-
-
-
+route.route("/:id")
+    .get(controller.getCustomerById)
+    .delete(controller.deleteCustomerById);
 
 module.exports = route;
