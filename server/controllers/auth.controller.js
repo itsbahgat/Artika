a37761e26357103a9144171ee0cf8d5d85a063cc
Youssef king <yussefbahgat@gmail.com>
2023-04-29 18:02:59 +0300

    Save token in cookies

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -10,7 +10,8 @@ const register = async (req, res) => {
     const newCustomer = await Users.create({
       ...req.body 
     });
-
+    const token = createToken(newCustomer.id);
+    res.cookie('jwt',token, {httpOnly: true, expiryTime: expiryTimeInSeconds});
     res.status(201).json({ message: 'Created Successfully', data: newCustomer });    
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -18,12 +19,12 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-
   try {
     const {emailOrUsername, password} = req.body;
-    let user = await Users.login(emailOrUsername,password);
+    const user = await Users.login(emailOrUsername,password); 
+    const token = createToken(user.id);
+    res.cookie('jwt',token, {httpOnly: true, expiryTime: expiryTimeInSeconds});
     res.status(200).json({ id: user.id });
-    //return user;
   } 
   catch (error) {
     res.status(401).json({ message: error.message });
@@ -31,12 +32,14 @@ const login = async (req, res) => {
 };
 
 const createToken = (id) => {
+    //const userRole = 
     return jwt.sign({ id }, privateKey, { expiresIn: expiryTimeInSeconds });
   };
   
 
 module.exports = {
     register,
-    login
+    login,
+    logout
 }
 
