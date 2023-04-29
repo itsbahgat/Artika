f2b73014ea6efdf7f91ba554b567036094992926
raneem <raneem74>
2023-04-29 19:15:41 +0300

    add angualr material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,8 +7,14 @@ import { AppComponent } from './app.component'
 
 import { FormsModule } from '@angular/forms';
 import { TagInputModule } from 'ngx-chips';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 const routes = [
@@ -26,7 +32,13 @@ const routes = [
     ComponentsModule,
     FormsModule,
     TagInputModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
