693ff87e872326eccd385191fde89857d56e6b33
Youssef king <yussefbahgat@gmail.com>
2023-05-06 02:42:47 +0300

    small fix

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
 import { ItemCard } from './item-card/item-card.component'
 import { NavbarComponent } from './navbar/navbar.component'
