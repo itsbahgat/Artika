91872332015f3a4dd1f74173c24968af618e2a73
Youssef king <yussefbahgat@gmail.com>
2023-05-08 03:39:00 +0300

    Add editCustomerById function and route

--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -32,8 +32,27 @@ let deleteCustomerById = async (req, res) => {
     }
 };
 
+let editCustomerById = async (req, res) => {
+    try{
+        const customerId = req.params.id;
+        const updatedData = req.body;
+        const updatedCustomer = await customerModel.findByIdAndUpdate(
+          customerId,
+          { $set: updatedData },
+          { new: true }
+        );
+
+         res.status(200).json({ message: "Updated Successfully" });
+      } catch (error) {
+        let isNotFound = error.name === "CastError";
+        if (isNotFound) res.status(404).json({ message: "Customer is not found" });
+        else res.status(400).json({ message: error.message });
+      }
+};
+
 module.exports = {
     getAllCustomers,
     getCustomerById,
+    editCustomerById,
     deleteCustomerById,
   };
