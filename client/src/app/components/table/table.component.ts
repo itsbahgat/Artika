e604590e18797fb99d3a8e3e4bd8368f4a7b29ff
Youssef king <yussefbahgat@gmail.com>
2023-05-08 18:15:26 +0300

    Add table component

---
+++ client/src/app/components/table/table.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent  {
+
+  tableData: any[] = [
+    {
+      customerName: 'Saeed Baik Chicken',
+      Address: '01115555555',
+      Title: 'XHSYUW100',
+      quantity: 50,
+      date:'12-6-2001',
+      status: 'Absent'
+    },
+    {
+      customerName: 'kmakel',
+      Address: '01115555555',
+      Title: 'XHSYUW100',
+      quantity: 50,
+      status: 'Absent'
+    },
+    {
+      customerName: 'Ranooma el Amora',
+      Address: '01115555555',
+      Title: 'XHSYUW100',
+      quantity: 50,
+      date:'12-6-2001',
+      status: 'present'
+    },
+    {
+      customerName: 'noura noura',
+      Address: '01115555555',
+      Title: 'XHSYUW100',
+      quantity: 50,
+      date:'12-6-2001',
+      status: 'Absent'
+    },
+    {
+      customerName: 'Ranooma el Amora',
+      Address: '01115555555',
+      Title: 'XHSYUW100',
+      quantity: 50,
+      date:'12-6-2001',
+      status: 'present'
+    },
+  ];
+
+  currentPage = 1;
+  itemsPerPage = 10;
+  totalPages = Math.ceil(this.tableData.length/this.itemsPerPage);
+
+  getStatusClass(status: string): string {
+    console.log(status);
+    const statusClasses = {
+      absent: 'status status-absent',
+      present: 'status status-present',
+      late: 'status status-late'
+    };
+  
+    status = status.toLowerCase();
+    return statusClasses[status] || '';
+  }
+  
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
+
+  getObjectValues(obj: any): any[] {
+    return Object.values(obj);
+  }
+}
+  
