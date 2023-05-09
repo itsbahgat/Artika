c6bdc08464b941f292fe5a1b743e57f90971d112
Youssef king <yussefbahgat@gmail.com>
2023-05-09 18:39:20 +0300

    Fix bug: seller schema inheritance

--- server/models/seller.model.js
+++ server/models/seller.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const user = require('./customer.model');
 
 const sellerSchema = new mongoose.Schema({
-    ...user.obj,
+    ...user.schema.obj,
     shop: {
       name: String,
       description: String,
