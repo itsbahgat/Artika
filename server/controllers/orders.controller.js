9730552b0ac9c4a64ffaf67224f207e179095b59
Noura Mahmoud <nouramahmoud099@gmail.com>
2023-05-10 19:47:14 +0300

    populate product details in GetOrderByCustId

--- server/controllers/orders.controller.js
+++ server/controllers/orders.controller.js
@@ -1,45 +1,54 @@
 const Orders = require("../models/order.model");
 
-
-module.exports.GetAllOrders = (request, response, next)=>{
-    Orders.find({}).then(data=>{
-        response.status(200).json(data);
-    }).catch(error=> {
-        next(error);});
+module.exports.GetAllOrders = (request, response, next) => {
+  Orders.find({})
+    .then((data) => {
+      response.status(200).json(data);
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-
-module.exports.GetOrderByCustId = (request, response, next)=>{
-    Orders.find({customerId: request.params.id}).then(data=>{
-        response.status(200).json(data);
-    }).catch(error=> {
-        next(error);});
+module.exports.GetOrderByCustId = (request, response, next) => {
+  Orders.find({ customerId: request.params.id })
+    .populate({
+      path: "items.productId",
+      model: "Product",
+    })
+    .then((data) => {
+      response.status(200).json(data);
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
+module.exports.UpdateOrder = async (request, response, next) => {
+  const { orderId, status } = request.body;
+  const updatedOrder = await Orders.findOne({ _id: orderId });
 
-module.exports.UpdateOrder = async(request, response, next)=>{
-    const { orderId, status } = request.body;
-    const updatedOrder = await Orders.findOne({ _id: orderId });
-  
-    if (!updatedOrder) {
-      return response
-        .status(404)
-        .json({ message: "There is no orders for this customer" });
-    }
-  
-    const oldState = updatedOrder.status;
-    updatedOrder.status = status;
+  if (!updatedOrder) {
+    return response
+      .status(404)
+      .json({ message: "There is no orders for this customer" });
+  }
 
-    if (oldState != status) {
-        updatedOrder.save()
-        .then(data=>{
-            response.status(200).json({
-                message: "order state was " + oldState + ", and now it is " + status,
-              });})
-        .catch(err=>next(err))
-    } else {
-      response.status(200).json({ message: "there is no change in status" });
-    }
-}
+  const oldState = updatedOrder.status;
+  updatedOrder.status = status;
+
+  if (oldState != status) {
+    updatedOrder
+      .save()
+      .then((data) => {
+        response.status(200).json({
+          message: "order state was " + oldState + ", and now it is " + status,
+        });
+      })
+      .catch((err) => next(err));
+  } else {
+    response.status(200).json({ message: "there is no change in status" });
+  }
+};
 
 module.exports.Orders = Orders;
