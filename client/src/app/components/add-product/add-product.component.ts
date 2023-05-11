cbcd3a4870a4e107a7d37e441e5e64d5c178092a
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2023-05-11 17:25:14 +0300

    adjusted spinner on adding products

--- client/src/app/components/add-product/add-product.component.ts
+++ client/src/app/components/add-product/add-product.component.ts
@@ -1,8 +1,14 @@
-import {Component, ViewEncapsulation, OnInit, ViewChild, ElementRef} from "@angular/core";
+import {
+  Component,
+  ViewEncapsulation,
+  OnInit,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 import { product } from "src/app/data-type";
 import { ProductService } from "src/app/pages/services/product.service";
 
-import { NgForm } from '@angular/forms';
+import { NgForm } from "@angular/forms";
 
 export interface AutoCompleteModel {
   value: any;
@@ -16,6 +22,8 @@ export interface AutoCompleteModel {
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AddProductComponent implements OnInit {
+  isLoading: boolean = false;
+
   ngOnInit(): void {}
 
   @ViewChild("fileInput", { static: false }) fileInput: ElementRef;
@@ -51,45 +59,57 @@ export class AddProductComponent implements OnInit {
 
   submit(addProduct: NgForm) {
     if (addProduct.valid) {
-      // Form is valid, submit data
+      // Set isLoading to true
+      this.isLoading = true;
+
       const data: product = addProduct.value;
-  
+
       const fileInput = this.fileInput.nativeElement;
       const files: FileList = fileInput.files;
-  
+
       const formData = new FormData();
       formData.append("title", data.title);
       formData.append("description", data.description);
       formData.append("price", data.price.toString());
       formData.append("seller", this.loggedSeller._id);
-  
+
       for (let i = 0; i < this.selectedCategories.length; i++) {
         formData.append("categories", this.selectedCategories[i]);
       }
-  
+
       const fileLimit = 4;
       const totalFiles = Math.min(files.length, fileLimit);
-  
+
       if (files.length > fileLimit) {
         const message = `You have selected ${files.length} files. Only the first ${fileLimit} files will be considered.`;
         alert(message);
       }
-  
+
       for (let i = 0; i < totalFiles; i++) {
         formData.append("images", files[i]);
       }
-  
-      this.productService.addProduct(formData).subscribe((data) => {
-        alert("Your product added successfully");
-      });
+
+      this.productService.addProduct(formData).subscribe(
+        (data) => {
+          // API call success
+          alert("Your product added successfully");
+          // Set isLoading back to false
+          this.isLoading = false;
+        },
+        (error) => {
+          // API call error
+          alert("An error occurred while adding the product");
+          // Set isLoading back to false
+          this.isLoading = false;
+        }
+      );
     } else {
       // Form is invalid
-      alert('Please enter valid data');
+      alert("Please enter valid data");
     }
   }
 
   onItemAdded(addedCat: any) {
     this.selectedCategories.push(addedCat.value);
   }
-
 }
