1d6c590bcfbde74c2e102ebe8efced958ce29f26
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2023-05-11 19:47:01 +0300

    home

--- client/src/app/components/category-card/category-card.component.ts
+++ client/src/app/components/category-card/category-card.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'category-card',
-  templateUrl: 'category-card.component.html',
-  styleUrls: ['category-card.component.css'],
+  selector: "category-card",
+  templateUrl: "category-card.component.html",
+  styleUrls: ["category-card.component.css"],
 })
 export class CategoryCard {
   @Input()
   category_img: string =
-    'https://images.unsplash.com/photo-1519947486511-46149fa0a254?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEyfHxjaGFpcnxlbnwwfHx8fDE2NTI4MTgxODk&ixlib=rb-1.2.1&w=1500'
+    "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1170104811.jpeg?resize=1200:*";
   @Input()
-  image_alt: string = 'image'
+  image_alt: string = "image";
   @Input()
-  name: string = 'Desks'
+  name: string = "Men";
   constructor() {}
 }
--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit , ChangeDetectorRef } from '@angular/core';
-import { AuthService } from '../../pages/services/authuser.service';
-import { CustomerService } from '../../pages/services/customer.service';
-
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { AuthService } from "../../pages/services/authuser.service";
+import { CustomerService } from "../../pages/services/customer.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
-
   firstName: string;
   lastName: string;
   email: string;
@@ -21,22 +19,30 @@ export class ProfileComponent implements OnInit {
 
   isEditMode: boolean = false; // Indicates whether the user is in edit mode
 
-
-  constructor(private authService: AuthService, private customerService : CustomerService, private cdr: ChangeDetectorRef) { }
+  constructor(
+    private authService: AuthService,
+    private customerService: CustomerService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     // Get user properties using AuthService
-    this.firstName = this.authService.getProperty('firstName') || 'N/A';
-    this.lastName = this.authService.getProperty('lastName') || 'N/A';
-    this.email = this.authService.getProperty('email') || 'N/A';
-    this.username = this.authService.getProperty('username') || 'N/A';
-    this.role = this.authService.getProperty('role') || 'N/A';
-    this.address = this.authService.getProperty('address') || 'N/A';
-    this.phone = this.authService.getProperty('phone') || 'N/A';
-    this.avatar = this.authService.getProperty('avatar') || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+    this.firstName = this.authService.getProperty("firstName") || "N/A";
+    this.lastName = this.authService.getProperty("lastName") || "N/A";
+    this.email = this.authService.getProperty("email") || "N/A";
+    this.username = this.authService.getProperty("username") || "N/A";
+    this.role = this.authService.getProperty("role") || "N/A";
+    this.address = this.authService.getProperty("address") || "N/A";
+    this.phone = this.authService.getProperty("phone") || "N/A";
+    this.avatar = this.authService.getProperty("avatar");
 
-    console.log ("first name",this.firstName);
-    console.log("avatar",this.avatar);
+    if (this.avatar === "undefined" || !this.avatar) {
+      this.avatar =
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+    }
+
+    console.log("first name", this.firstName);
+    console.log("avatar", this.avatar);
   }
 
   toggleEditMode() {
@@ -44,52 +50,61 @@ export class ProfileComponent implements OnInit {
     this.cdr.detectChanges();
   }
 
-
   updateProfile() {
     if (this.isEditMode) {
       this.toggleEditMode();
-  
-      const emailInput = document.querySelector('#emailInput') as HTMLInputElement;
-      const phoneInput = document.querySelector('#phoneInput') as HTMLInputElement;
-      const addressInput = document.querySelector('#addressInput') as HTMLInputElement;
-  
+
+      const emailInput = document.querySelector(
+        "#emailInput"
+      ) as HTMLInputElement;
+      const phoneInput = document.querySelector(
+        "#phoneInput"
+      ) as HTMLInputElement;
+      const addressInput = document.querySelector(
+        "#addressInput"
+      ) as HTMLInputElement;
+
       const updatedEmail = emailInput.value || this.email;
       const updatedPhone = phoneInput.value || this.phone;
       const updatedAddress = addressInput.value || this.address;
-  
+
       // Check if the values have changed before calling the update
-      if (updatedEmail !== this.email || updatedPhone !== this.phone || updatedAddress !== this.address) {
+      if (
+        updatedEmail !== this.email ||
+        updatedPhone !== this.phone ||
+        updatedAddress !== this.address
+      ) {
         this.updateCustomerData(updatedEmail, updatedPhone, updatedAddress);
       }
     } else {
       this.toggleEditMode();
     }
   }
-  
 
-updateCustomerData(updatedEmail: string, updatedPhone: string, updatedAddress: string) {
-  // Create an object with the updated customer data
-  const updatedCustomerData = {
-    email: updatedEmail,
-    phone: updatedPhone,
-    address: updatedAddress
-  };
+  updateCustomerData(
+    updatedEmail: string,
+    updatedPhone: string,
+    updatedAddress: string
+  ) {
+    // Create an object with the updated customer data
+    const updatedCustomerData = {
+      email: updatedEmail,
+      phone: updatedPhone,
+      address: updatedAddress,
+    };
 
-  // Call the updateCustomer service method and pass the customer ID and updated data
-  this.customerService.updateCustomer(this.authService.getProperty("_id"), updatedCustomerData)
-    .subscribe(
-      response => {
-        // Handle the successful response from the service if needed
-        console.log('Customer updated successfully:', response);
-      },
-      error => {
-        // Handle any errors that occurred during the update process
-        console.error('Error updating customer:', error);
-      }
-    );
-}
-
-  
-  
-  
+    // Call the updateCustomer service method and pass the customer ID and updated data
+    this.customerService
+      .updateCustomer(this.authService.getProperty("_id"), updatedCustomerData)
+      .subscribe(
+        (response) => {
+          // Handle the successful response from the service if needed
+          console.log("Customer updated successfully:", response);
+        },
+        (error) => {
+          // Handle any errors that occurred during the update process
+          console.error("Error updating customer:", error);
+        }
+      );
+  }
 }
