3a209b7bb7e42bf182e5718450e5f96480e82467
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2023-05-11 10:33:48 +0300

    payment fixed and loading added

--- client/src/app/components/payment/payment.component.ts
+++ client/src/app/components/payment/payment.component.ts
@@ -1,70 +1,81 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
-import { pay } from '../../pages/pay/pay.component';
+import { Component, Input, ViewChild } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { pay } from "../../pages/pay/pay.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  selector: "app-payment",
+  templateUrl: "./payment.component.html",
+  styleUrls: ["./payment.component.css"],
 })
-export class PaymentComponent{
+export class PaymentComponent {
+  @Input() customerID: string = "";
+  @Input() total: any;
+  @ViewChild("payRef") payRef!: pay;
 
-  @Input() customerID: string = '';
-  @Input() total: any ;
-  @ViewChild('payRef') payRef!: pay;
-  
   paymentData = {
-    amount : 0 ,
-    currency: 'USD',
+    amount: 0,
+    currency: "USD",
     source: {
-      object: 'Visa',
-      number: '',
+      object: "Visa",
+      number: "",
       exp_month: 0,
       exp_year: 0,
-      cvc: ''
+      cvc: "",
     },
-    description: 'description'
+    description: "description",
   };
-  
-  //possibleCurrencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "INR", "ISK", "JMD", "JPY", "KES", "KGS", "KHR", "KMF", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD", "STN", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XOF", "XPF", "YER", "ZAR", "ZMW"];
 
+  isLoading: boolean = false; // Flag to indicate loading state
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  
   onSubmit(): void {
+    this.isLoading = true; // Set loading state to true
+
     this.paymentData.amount = Math.ceil(+this.total);
-    this.paymentData.source.exp_month = Number(this.paymentData.source.exp_month);
+    this.paymentData.source.exp_month = Number(
+      this.paymentData.source.exp_month
+    );
     this.paymentData.source.exp_year = Number(this.paymentData.source.exp_year);
 
-    this.http.post('http://localhost:3005/stripe', this.paymentData)
-      .subscribe((response) => {
-        console.log("payment :",this.paymentData);
-        let checkoutData = {
-          customerId: this.customerID, 
-          checkout:true
+    this.http.post("http://localhost:3005/stripe", this.paymentData).subscribe(
+      (response) => {
+        console.log("payment :", this.paymentData);
+        const checkoutData = {
+          customerId: this.customerID,
+          checkout: true,
         };
-        const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        this.http.delete('http://localhost:3005/cart', { headers, body: checkoutData })
-        .subscribe((res) => {
-          console.log(res);
-          alert('Your order was submitted');
-          },
-          (err) => {
-            console.error('Error:', err);
-            alert('Error: Please check over order again');
-          });
+        const headers = new HttpHeaders().set(
+          "Content-Type",
+          "application/json"
+        );
+
+        this.http
+          .delete("http://localhost:3005/cart", { headers, body: checkoutData })
+          .subscribe(
+            (res) => {
+              console.log(res);
+              alert("Your order was submitted");
+              this.router.navigate(["/"]); // Redirect to success page
+            },
+            (err) => {
+              console.error("Error:", err);
+              alert("Error: Please check your order again");
+            }
+          );
       },
       (error) => {
         if (error.status !== 200) {
-          console.error('Error:', error);
-          alert('Error: Please check over your data again');
+          console.error("Error:", error);
+          alert("Error: Please check your data again");
         }
+      },
+      () => {
+        this.isLoading = false; // Set loading state to false after HTTP request completes
       }
-      );
+    );
   }
-
 }
