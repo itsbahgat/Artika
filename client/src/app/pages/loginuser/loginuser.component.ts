10b79415d3033a4217dbedab6111eee2beb93fea
kishk <mrsaytama000@gmail.com>
2023-05-11 16:29:04 +0300

    present validation error in front end, files edited(loginuser.component.ts,register.component.html,register.component.ts)

--- client/src/app/pages/loginuser/loginuser.component.ts
+++ client/src/app/pages/loginuser/loginuser.component.ts
@@ -1,40 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/authuser.service';
-import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
-
-
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/authuser.service";
+import { Router } from "@angular/router";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'app-loginuser',
-  templateUrl: 'loginuser.component.html',
-  styleUrls: ['loginuser.component.css'],
+  selector: "app-loginuser",
+  templateUrl: "loginuser.component.html",
+  styleUrls: ["loginuser.component.css"],
   // ...
 })
 export class loginuser implements OnInit {
   messageError: string;
   err: HttpErrorResponse;
-  constructor(private authService: AuthService,private route:Router) {}
+  constructor(private authService: AuthService, private route: Router) {}
 
   ngOnInit(): void {}
 
   onLogin(emailOrUsername: string, password: string) {
-    this.authService.login(emailOrUsername, password)
-      .subscribe(
-        // handle successful response here
-        response => {
-          console.log(response);
-          this.route.navigate(['/']).then(() => {
-        location.replace(location.href);
-      });
-        },
-        // handle error response here
-        error => {
-          
-          this.messageError = error.message; // set the messageError property with the error message
-          console.error(this.messageError);
-        },
-       
-      );
+    this.authService.login(emailOrUsername, password).subscribe(
+      // handle successful response here
+      (response) => {
+        console.log(response);
+        this.route.navigate(["/"]).then(() => {
+          location.replace(location.href);
+        });
+      },
+      // handle error response here
+      (error) => {
+        console.log(error.error.message);
+        this.messageError = error.error.message; // set the messageError property with the error message
+        console.error(this.messageError);
+      }
+    );
   }
 }
