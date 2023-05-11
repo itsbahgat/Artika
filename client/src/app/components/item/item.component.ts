7874b935cd70c2e42654903c75aa4612adf6badd
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2023-05-11 17:53:05 +0300

    seller service

--- client/src/app/components/item/item.component.ts
+++ client/src/app/components/item/item.component.ts
@@ -1,93 +1,100 @@
-import { Component, Input, OnInit , AfterViewInit,AfterContentInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CartService } from '../../pages/services/cart.service';
-import { CustomerService } from '../../pages/services/customer.service';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit, AfterViewInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { CartService } from "../../pages/services/cart.service";
+import { CustomerService } from "../../pages/services/customer.service";
+import { SellerService } from "../../pages/services/seller.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  selector: "app-item",
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.css"],
 })
 export class ItemComponent implements AfterViewInit {
-  @Input() product:any= {};
+  @Input() product: any = {};
   @Input() customerId: string;
-  @Input() productID : string;
+  @Input() productID: string;
 
-  constructor(private cartService: CartService, private http: HttpClient, private customerService: CustomerService ,private router: Router) {
-  }
+  constructor(
+    private cartService: CartService,
+    private http: HttpClient,
+    private customerService: CustomerService,
+    private router: Router,
+    private sellerService: SellerService
+  ) {}
 
   ngAfterViewInit() {
-      this.getSellerById();
-      this.getReviewUserById();
+    this.getSellerById();
+    this.getReviewUserById();
   }
-  
+
   getSellerById() {
-    let id = this.product.seller;
-    this.customerService.getCustomerById(id)
-      .subscribe(
-        (data) => {
-          const nameReturned = data;
-          console.log(nameReturned);
-          this.product.sellerName = nameReturned.username;
-        },
-        (error) => {
-          console.log('Error fetching customer data', error);
-        }
-      );
-  }
-
-  getReviewUserById() {
-    this.product.reviews.forEach(review => {
-      this.customerService.getCustomerById(review.user)
-        .subscribe(
-          (data) => {
-            const nameReturned = data;
-            console.log("hey",nameReturned.username);
-            review.username = nameReturned.username;
-          },
-          (error) => {
-            console.log('Error fetching customer data', error);
-          }
-        );
-    });
-  }
-  
-  
-
-  addToCart(): void {
-    console.log("productID:" , this.productID)
-    console.log("customerID:" , this.customerId)
-    
-    this.cartService.addProductToCart(this.customerId, this.productID).subscribe(
-      (response) => {
-        console.log('Successfully added item to cart!', response);
-        if (this.confirmAction()) {
-          this.router.navigateByUrl('/cart');
-        }
-        // TODO: Display success message to user
+    const id = this.product.seller;
+    this.sellerService.getSellerById(id).subscribe(
+      (data) => {
+        const nameReturned = data;
+        console.log(nameReturned[0].username);
+        this.product.sellerName = nameReturned[0].username;
       },
       (error) => {
-        console.error('Error adding item to cart', error);
-        // TODO: Display error message to user
+        console.log("Error fetching customer data", error);
       }
     );
   }
 
-  confirmAction(): boolean {
-    return window.confirm('Item Succesfully added , Do you wanna go to cart?');
+  getReviewUserById() {
+    this.product.reviews.forEach((review) => {
+      this.customerService.getCustomerById(review.user).subscribe(
+        (data) => {
+          const nameReturned = data;
+          console.log("hey", nameReturned.username);
+          review.username = nameReturned.username;
+        },
+        (error) => {
+          console.log("Error fetching customer data", error);
+        }
+      );
+    });
   }
-  
+
+  addToCart(): void {
+    console.log("productID:", this.productID);
+    console.log("customerID:", this.customerId);
+
+    this.cartService
+      .addProductToCart(this.customerId, this.productID)
+      .subscribe(
+        (response) => {
+          console.log("Successfully added item to cart!", response);
+          if (this.confirmAction()) {
+            this.router.navigateByUrl("/cart");
+          }
+          // TODO: Display success message to user
+        },
+        (error) => {
+          console.error("Error adding item to cart", error);
+          // TODO: Display error message to user
+        }
+      );
+  }
+
+  confirmAction(): boolean {
+    return window.confirm(
+      "Item Successfully added. Do you want to go to the cart?"
+    );
+  }
 
   currentImageIndex = 0;
 
-
   prevImage() {
-    this.currentImageIndex = (this.currentImageIndex - 1 + this.product.images.length) % this.product.images.length;
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.product.images.length) %
+      this.product.images.length;
   }
 
   nextImage() {
-    this.currentImageIndex = (this.currentImageIndex + 1) % this.product.images.length;
+    this.currentImageIndex =
+      (this.currentImageIndex + 1) % this.product.images.length;
   }
 
   getTimeAgo(dateString: string): string {
@@ -97,17 +104,15 @@ export class ItemComponent implements AfterViewInit {
     const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
     const diffHrs = Math.floor(diffMs / (1000 * 60 * 60));
     const diffMins = Math.floor(diffMs / (1000 * 60));
-  
+
     if (diffDays > 0) {
-      return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+      return `${diffDays} day${diffDays > 1 ? "s" : ""} ago`;
     } else if (diffHrs > 0) {
-      return `${diffHrs} hour${diffHrs > 1 ? 's' : ''} ago`;
+      return `${diffHrs} hour${diffHrs > 1 ? "s" : ""} ago`;
     } else if (diffMins > 0) {
-      return `${diffMins} minute${diffMins > 1 ? 's' : ''} ago`;
+      return `${diffMins} minute${diffMins > 1 ? "s" : ""} ago`;
     } else {
-      return 'just now';
+      return "just now";
     }
   }
-
-
 }
--- client/src/app/pages/services/seller.service.ts
+++ client/src/app/pages/services/seller.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SellerService {
-  private baseUrl = 'http://localhost:3005'; // Update the base URL accordingly
+  private baseUrl = "http://localhost:3005"; // Update the base URL accordingly
 
   constructor(private http: HttpClient) {}
+
+  getSellerById(sellerId: string): Observable<any> {
+    const url = `${this.baseUrl}/seller/details/${sellerId}`;
+    return this.http.get(url);
+  }
 
   updateOrders(orders: any[]): Observable<any> {
     const url = `${this.baseUrl}/seller/update-orders`;
